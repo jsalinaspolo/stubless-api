a68680d9ed0d70486e1066710f7d549032e994c6
javiersalinas <javiersalinas@ucds.email>
2017-12-05 16:34:01 +0000

    Add learning tests for aws SDK client.

--- src/main/java/com/stublessapi/learning/AmazonApiBuild.java
+++ src/main/java/com/stublessapi/learning/AmazonApiBuild.java
@@ -2,53 +2,78 @@ package com.stublessapi.learning;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.apigateway.AmazonApiGateway;
 import com.amazonaws.services.apigateway.AmazonApiGatewayClientBuilder;
+import com.amazonaws.services.apigateway.model.AuthorizerType;
 import com.amazonaws.services.apigateway.model.CreateApiKeyRequest;
 import com.amazonaws.services.apigateway.model.CreateApiKeyResult;
 import com.amazonaws.services.apigateway.model.CreateModelRequest;
 import com.amazonaws.services.apigateway.model.CreateResourceRequest;
+import com.amazonaws.services.apigateway.model.CreateResourceResult;
 import com.amazonaws.services.apigateway.model.CreateRestApiRequest;
 import com.amazonaws.services.apigateway.model.CreateRestApiResult;
+import com.amazonaws.services.apigateway.model.DeleteRestApiRequest;
+import com.amazonaws.services.apigateway.model.GatewayResponseType;
+import com.amazonaws.services.apigateway.model.GetMethodRequest;
+import com.amazonaws.services.apigateway.model.GetResourceRequest;
+import com.amazonaws.services.apigateway.model.GetResourcesRequest;
+import com.amazonaws.services.apigateway.model.GetResourcesResult;
+import com.amazonaws.services.apigateway.model.PutGatewayResponseRequest;
+import com.amazonaws.services.apigateway.model.PutIntegrationRequest;
+import com.amazonaws.services.apigateway.model.PutIntegrationResponseRequest;
+import com.amazonaws.services.apigateway.model.PutMethodRequest;
+import com.amazonaws.services.apigateway.model.PutMethodResponseRequest;
 import org.apache.http.entity.ContentType;
+
+import java.util.UUID;
 
 public class AmazonApiBuild {
 
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        AmazonApiGateway client = AmazonApiGatewayClientBuilder.standard().withCredentials(new AWSCredentialsProvider() {
-            @Override
-            public AWSCredentials getCredentials() {
-                return new AWSCredentials() {
-                    @Override
-                    public String getAWSAccessKeyId() {
-                        return "key";
-                    }
+    AmazonApiGateway client = AmazonApiGatewayClientBuilder.standard()
+      .withCredentials(new SystemPropertiesCredentialsProvider())
+      .withRegion(Regions.EU_WEST_1).build();
 
-                    @Override
-                    public String getAWSSecretKey() {
-                        return "secret";
-                    }
-                };
-            }
+    String randomAPI = UUID.randomUUID().toString().substring(0, 8);
+    CreateRestApiResult restApi = client.createRestApi(new CreateRestApiRequest().withName("restTest-" + randomAPI));
 
-            @Override
-            public void refresh() {
+    GetResourcesResult resources = client.getResources(new GetResourcesRequest().withRestApiId(restApi.getId()));
 
-            }
-        }).withRegion(Regions.EU_WEST_1).build();
+    CreateResourceResult stub = client.createResource(new CreateResourceRequest()
+      .withParentId(resources.getItems().get(0).getId())
+      .withPathPart("stub")
+      .withRestApiId(restApi.getId())
 
-//        CreateApiKeyResult apiKey = client.createApiKey(new CreateApiKeyRequest().withName("test")
-//                .withValue("12345678901234567890"));
-        CreateRestApiResult restApi = client.createRestApi(new CreateRestApiRequest().withName("restTest"));
-        client.createResource(new CreateResourceRequest()
-                .withParentId("/some")
-                .withPathPart("test-endpoint")
-                .withRestApiId(restApi.getId())
-        );
+    );
 
-        System.out.println("hi");
-    }
+    client.putMethod(new PutMethodRequest()
+      .withResourceId(stub.getId())
+      .withHttpMethod("GET")
+      .withAuthorizationType("NONE")
+      .withRestApiId(restApi.getId()));
+
+
+//    client.putMethodResponse(new PutMethodResponseRequest()
+//      .withStatusCode("200")
+//      .withRestApiId(restApi.getId())
+//      .withHttpMethod("GET")
+//      .withResourceId(stub.getId())
+//      .withResponseModels()
+//    );
+
+    client.putGatewayResponse(new PutGatewayResponseRequest()
+      .withResponseType(GatewayResponseType.UNAUTHORIZED)
+      .withStatusCode("200")
+      .withRestApiId(restApi.getId())
+    );
+//    client.putIntegrationResponse(new PutIntegrationResponseRequest()
+//      .withRestApiId(restApi.getId()));
+
+//    client.deleteRestApi(new DeleteRestApiRequest().withRestApiId(restApi.getId()));
+    System.out.println("hi");
+  }
 }
--- src/test/java/com/stublessapi/learning/AmazonApiBuildShould.java
+++ src/test/java/com/stublessapi/learning/AmazonApiBuildShould.java
@@ -1,5 +1,80 @@
 package com.stublessapi.learning;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.apigateway.AmazonApiGateway;
+import com.amazonaws.services.apigateway.AmazonApiGatewayClientBuilder;
+import com.amazonaws.services.apigateway.model.CreateResourceRequest;
+import com.amazonaws.services.apigateway.model.CreateResourceResult;
+import com.amazonaws.services.apigateway.model.CreateRestApiRequest;
+import com.amazonaws.services.apigateway.model.CreateRestApiResult;
+import com.amazonaws.services.apigateway.model.DeleteRestApiRequest;
+import com.amazonaws.services.apigateway.model.GetResourcesRequest;
+import com.amazonaws.services.apigateway.model.GetRestApisRequest;
+import com.amazonaws.services.apigateway.model.PutMethodRequest;
+import com.amazonaws.services.apigateway.model.PutMethodResult;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class AmazonApiBuildShould {
 
+  private AmazonApiGateway client = AmazonApiGatewayClientBuilder.standard()
+//    .withCredentials(new SystemPropertiesCredentialsProvider())
+    .withCredentials(new ProfileCredentialsProvider("jsalinas"))
+    .withRegion(Regions.EU_WEST_1).build();
+
+  private CreateRestApiResult restApi;
+
+  @Before
+  public void initialise() {
+    String randomAPI = UUID.randomUUID().toString().substring(0, 8);
+    restApi = client.createRestApi(new CreateRestApiRequest().withName("n-restTest-" + randomAPI));
+  }
+
+  @After
+  public void clean() {
+    client.getRestApis(new GetRestApisRequest())
+      .getItems().
+      forEach(api -> client.deleteRestApi(new DeleteRestApiRequest().withRestApiId(api.getId())));
+  }
+
+  @Test
+  public void createResourceFromRootParent() {
+    String rootResourceId = getRootResourceId();
+
+    CreateResourceResult stub = client.createResource(new CreateResourceRequest()
+      .withParentId(rootResourceId)
+      .withPathPart("stub")
+      .withRestApiId(restApi.getId())
+    );
+
+    assertThat(stub.getPath()).isEqualTo("/stub");
+  }
+
+  @Test
+  public void createMethodForAResource() {
+    CreateResourceResult stub = client.createResource(new CreateResourceRequest()
+      .withParentId(getRootResourceId())
+      .withPathPart("stub")
+      .withRestApiId(restApi.getId())
+    );
+
+    PutMethodResult putMethodResult = client.putMethod(new PutMethodRequest()
+      .withResourceId(stub.getId())
+      .withHttpMethod("GET")
+      .withAuthorizationType("NONE")
+      .withRestApiId(restApi.getId()));
+
+    System.out.println(putMethodResult);
+  }
+
+  private String getRootResourceId() {
+    return client.getResources(new GetResourcesRequest().withRestApiId(restApi.getId())).getItems().get(0).getId();
+  }
 }
