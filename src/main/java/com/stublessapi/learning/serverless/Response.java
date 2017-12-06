6bed04be88d727610f6add8609e57583bd54fae5
MuSTa1nE <mustainer@gmail.com>
2017-12-06 23:20:04 +0000

    Add example using serverless.

---
+++ src/main/java/com/stublessapi/learning/serverless/Response.java
@@ -0,0 +1,10 @@
+package com.stublessapi.learning.serverless;
+
+public class Response {
+
+  public final String message;
+
+  public Response(String message) {
+    this.message = message;
+  }
+}
